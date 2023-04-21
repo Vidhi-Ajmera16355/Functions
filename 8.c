8d6a25daf29b1e2389b67aad394ddfa79bb58361
Vidhi Ajmera <113877644+Vidhi-Ajmera16355@users.noreply.github.com>
2023-04-21 23:58:35 +0530

    Create 8.c

---
+++ 8.c
@@ -0,0 +1,37 @@
+#include <stdio.h> 
+int isArmstrong(int num)
+{
+    int temp, lastDigit, sum;
+    temp = num;
+    sum = 0;
+    while(temp != 0)
+    {
+        lastDigit = temp % 10;
+        sum += lastDigit * lastDigit * lastDigit;
+        temp /= 10;
+    }
+    if(num == sum)
+        return 1;
+    else 
+        return 0;
+}
+void printArmstrong(int start, int end){
+    while(start <= end){
+        if(isArmstrong(start)) 
+        {
+            printf("%d, ", start);
+        }
+        
+        start++; }}
+int main()
+{
+    int start, end;
+    printf("Enter lower limit to print armstrong numbers: ");
+    scanf("%d", &start);
+    printf("Enter upper limit to print armstrong numbers: ");
+    scanf("%d", &end);
+    printf("All armstrong numbers between %d to %d are: \n", start, end);
+    printArmstrong(start, end);
+    
+    return 0;
+}
