fc23792212da201ae3ba1d40a9b01e16297feafe
Vidhi Ajmera <113877644+Vidhi-Ajmera16355@users.noreply.github.com>
2023-04-21 10:08:23 +0530

    Create 2.c

---
+++ 2.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int circle(int r){
+    int d=2*r;
+    printf("Diameter is : %d\n",d);
+    int c=2*3.14*r;
+    printf("Circumference is : %d\n",c);
+    int a=3.14*r*r;
+    printf("Area is : %d\n",a);
+}
+int main()
+{
+    int r;
+    printf("Enter radius of circle:\n");
+    scanf("%d",&r);
+   int k = circle(r);
+   printf("%d",k);
+    return 0;
+}
